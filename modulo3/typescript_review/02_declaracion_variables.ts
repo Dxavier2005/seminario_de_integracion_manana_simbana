cd07c195e7ec32b02ff7ec0f5d5daca0cbf048e4
Dxavier2005 <dariosimbanasalcan@gmail.com>
2026-06-24 05:59:39 -0500

    06-24-2026

---
+++ modulo3/typescript_review/02_declaracion_variables.ts
@@ -0,0 +1,26 @@
+// const — valor que NO cambia (preferida por defecto)
+const PI: number = 3.14159;
+const NOMBRE_APP: string = "InventarioApp";
+const DEBUG_MODE: boolean = false;
+
+// let — valor que SÍ puede cambiar
+let contador: number = 0;
+let estadoConexion: string = "desconectado";
+let usuarioActivo: boolean = false;
+console.log(`contador: ${contador}, 
+    estadoConexion: ${estadoConexion}, 
+    usuarioActivo: ${usuarioActivo}`);
+
+contador++;                         // 1
+estadoConexion = "conectado";       // ok
+usuarioActivo = true;               // ok
+
+console.log(`contador: ${contador}, 
+    estadoConexion: ${estadoConexion}, 
+    usuarioActivo: ${usuarioActivo}`);
+
+
+// PI = 3;  // ← Error: Cannot assign to 'PI' because it is a constant.
+
+
+
